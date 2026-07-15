85a3cf3bf41056be0aa284e2288cdde778cf8b6a
Nico F. <nico01f@gmail.com>
2026-07-15 16:09:00 -0400

    fix(rails): allow 'app' hostname and remove duplicate migration
    - config.hosts << 'app': allows requests from Docker service name 'app'
    - Remove 202606200000001 migration (duplicate, timestamp invalid: 15 digits)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,4 +5,5 @@ Rails.application.configure do
   config.eager_load = false
   config.consider_all_requests_local = true
   config.public_file_server.enabled = true
+  config.hosts << "app"
 end
--- db/migrate/202606200000001_add_password_and_phone_verification_to_users.rb
+++
@@ -1,8 +0,0 @@
-class AddPasswordAndPhoneVerificationToUsers < ActiveRecord::Migration[8.0]
-  def change
-    add_column :users, :password_digest, :string
-    add_column :users, :phone_verified, :boolean, default: false
-    add_column :users, :phone_verification_token, :string
-    add_column :users, :phone_verified_at, :datetime
-  end
-end
